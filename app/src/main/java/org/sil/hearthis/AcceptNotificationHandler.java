7f8c0b71d94c8bb72152927448f5db3ce8371bdb
Major-Q <63882792+Major-Q@users.noreply.github.com>
2026-03-03 11:46:10 -0700

    Update http server
    Updated the server from the old library to the new library nanohttpd

--- app/src/main/java/org/sil/hearthis/AcceptNotificationHandler.java
+++ app/src/main/java/org/sil/hearthis/AcceptNotificationHandler.java
@@ -1,23 +1,18 @@
 package org.sil.hearthis;
-import android.content.Context;
-import org.apache.http.HttpException;
-import org.apache.http.HttpRequest;
-import org.apache.http.HttpResponse;
-import org.apache.http.entity.StringEntity;
-import org.apache.http.protocol.HttpContext;
-import org.apache.http.protocol.HttpRequestHandler;
-import java.io.IOException;
+
 import java.util.ArrayList;
+import fi.iki.elonen.NanoHTTPD;
+import fi.iki.elonen.NanoHTTPD.Response;
 
 /**
  * Created by Thomson on 1/18/2016.
  */
-public class AcceptNotificationHandler implements HttpRequestHandler {
+public class AcceptNotificationHandler {
 
     public interface NotificationListener {
         void onNotification(String message);
     }
-    static ArrayList<NotificationListener> notificationListeners= new ArrayList<NotificationListener>();
+    static ArrayList<NotificationListener> notificationListeners = new ArrayList<NotificationListener>();
 
     public static void addNotificationListener(NotificationListener listener) {
         notificationListeners.add(listener);
@@ -27,14 +22,12 @@ public class AcceptNotificationHandler implements HttpRequestHandler {
         notificationListeners.remove(listener);
     }
 
-    @Override
-    public void handle(HttpRequest request, HttpResponse response, HttpContext httpContext) throws HttpException, IOException {
-
+    public Response handle(NanoHTTPD.IHTTPSession session) {
         // Enhance: allow the notification to contain a message, and pass it on.
         // The copy is made because the onNotification calls may well remove listeners, leading to concurrent modification exceptions.
-        for (NotificationListener listener: notificationListeners.toArray(new NotificationListener[notificationListeners.size()])) {
+        for (NotificationListener listener : notificationListeners.toArray(new NotificationListener[notificationListeners.size()])) {
             listener.onNotification("");
         }
-        response.setEntity(new StringEntity("success"));
+        return NanoHTTPD.newFixedLengthResponse(Response.Status.OK, NanoHTTPD.MIME_PLAINTEXT, "success");
     }
 }
